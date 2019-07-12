f7255b61237f2254f18cd035322be515cd130fa3
Tessa Anderso <quintessa@8thlight.com>
2019-07-12 11:58:17 -0500

    Add additional tests Working on CI setup

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,10 +1,22 @@
 require 'example'
 
 RSpec.describe Factorial do
-  it "calculates the factorial of 5" do
-    calculator = Factorial.new
+  let(:calculator) { Factorial.new }
 
+  it "calculates the factorial of 5" do
     expect(calculator.factorial_of(5)).to eq(120)
 
+  end
+
+  it "calculates the factorial of 10" do
+    expect(calculator.factorial_of(10)).to eq(3628800)
+  end
+
+  it "calculates the factorial of 8" do
+    expect(calculator.factorial_of(8)).to eq(40320)
+  end
+
+  it "calculates the factorial of 50" do
+    expect(calculator.factorial_of(10)).to eq(3628800)
   end
 end
